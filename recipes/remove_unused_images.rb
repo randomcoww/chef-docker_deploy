e301ff5734801aa35db619e2fdaff80718470588
randomcoww <etherblimp@gmail.com>
2015-02-20 22:06:10 +0000

    image cleanup message tweak

--- recipes/remove_unused_images.rb
+++ recipes/remove_unused_images.rb
@@ -5,6 +5,7 @@ class Chef::Resource
 ## remove any images not referenced by a container or another image
 ruby_block "remove_unused_images" do
   block do
+    puts "Removing unused images"
     list_all_images.each do |i_id|
       begin
         docker_rmi(i_id)
