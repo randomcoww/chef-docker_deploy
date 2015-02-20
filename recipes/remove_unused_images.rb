1fd57fa644d9e2a9afc3943480ae13161b95125c
randomcoww <etherblimp@gmail.com>
2015-02-20 01:18:18 +0000

    output tweak

--- recipes/remove_unused_images.rb
+++ recipes/remove_unused_images.rb
@@ -8,8 +8,9 @@ ruby_block "remove_unused_images" do
     list_all_images.each do |i_id|
       begin
         docker_rmi(i_id)
+        puts "Image deleted: #{i_id}"
       rescue
-        puts "Image #{i_id} in use"
+        puts "Image in use: #{i_id}"
       end
     end
   end
